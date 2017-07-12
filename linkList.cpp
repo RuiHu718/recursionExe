9f85d91a78796f9e1c5fcf6ea6d0356d5224b705
Rui Hu <rui.hu6@icloud.com>
2017-07-12 17:06:56 +0800

    up to but not include problem No.10

--- linkList.cpp
+++ linkList.cpp
@@ -18,6 +18,13 @@
    2. getNth(): return the nth element of the list
    3. deleteList(): deallocates all the memory and sets head to null
    4. pop():    return head node data, delete head node, makes head node point to next
+   5. insertNth():
+   6. sortedInsert(): write the iterative one when have more time
+   7. insertSort():   given a list and make it into sorted order
+   8. append(): do this recursively as well
+   9. frontBackSplit(): 
+   10 removeDuplicates();
+   x. Professor Z's remove element code
  */
 
 
@@ -52,20 +59,25 @@ void deleteList(node *head);
 void recDeleteList(node *head);
 int pop(node* & head);
 void insertNth(node* & head, int index, int data);
+void sortedInsert(node* & head, node* newNode);
+void insertSort(node* & head);
+void append(node* & a, node* & b);
+void frontBackSplit(node *source, node* & front, node* & back);
+void removeDuplicates(node *head);
+
 
 int main()
 {
 
   node *testList = NULL;
+  //node *testList2 = NULL;
   
-  //testList = buildOneTwoThree();
-  insertNth(testList, 0, 13);
-  insertNth(testList, 0, 100);
-
-  insertNth(testList, 1, 42);
+  testList = buildOneTwoThree();
+  //testList2 = buildOneTwoThree();
   
-  insertNth(testList, 1, 5);
-  insertNth(testList, 4, 27);
+  push(testList, 0);
+  push(testList, 0);
+  removeDuplicates(testList);
   printList(testList);
 
 
@@ -319,5 +331,84 @@ void insertNth(node* & head, int index, int data)
   //     (cur -> next) -> next = temp;
   //   }
   // }
+  
+}  
+
+/* given a sorted list and a new node, add the new node to the proper place
+ * try use recursion here 
+ */
+void sortedInsert(node* & head, node* newNode)
+{
+  if (head == NULL || (newNode -> data) < (head -> data)) {		
+    newNode -> next = head;
+    head = newNode;
+  } else {
+      sortedInsert(head -> next, newNode);
+  }
+}  
+
+
+/* insertSort(): sort list into order 
+ * uses sortedInsert function above  
+ */
+void insertSort(node* & head)
+{
+  node* newList = NULL;
+  node* cur = NULL;
+  node* temp = NULL;
+  
+  for (cur = head; cur != NULL; cur = temp) { // a while loop is probably better
+    temp = cur -> next;			      // note why this is here
+    sortedInsert(newList, cur);
+  }
+
+  head = newList;
+}  
+
+
+/* appends one list after the other
+ * pay attention to special case where a starts empty
+ */
+void append(node* & a, node* & b)
+{
+  node* cur = NULL;
+  if (a == NULL) {		// special case
+    a = b;
+  } else {
+    for (cur = a; (cur -> next) != NULL; cur = cur -> next) {
+      // nothing here, simply to advance the pointer to NULL position
+    }
+    cur -> next = b;		// you have to draw pictures, otherwise you will
+				// make mistakes like this
+  }
+  b = NULL;
+}  
+
+
+/* splits the list in half, if list has odd number of cells 
+ * then the front list will have one more element
+ */
+void frontBackSplit(node *source, node* & front, node* & back)
+{
+  if (length(source) < 2) {	// special case
+    front = source;		// assumption: original value of back is NULL
+  } else {
+    node* cur = source;
+    int split = (length(source) - 1) / 2;
+    for (int i = 0; i < split; i++) {
+      cur = cur -> next;
+    }
+    front = source;
+    back = cur -> next;
+    cur -> next = NULL;
+  }
+}  
+
+
+/* Removes duplicates of a sorted list */
+void removeDuplicates(node *head)
+{
+
+
   
 }  
