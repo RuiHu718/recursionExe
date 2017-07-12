0315ad80535cf17545c18aac6f70e9deb0949b78
Rui Hu <rui.hu6@icloud.com>
2017-07-12 10:54:30 +0800

    Nick's linked list problems, up to No.5

--- linkList.cpp
+++ linkList.cpp
@@ -7,8 +7,18 @@
    6. printList();
    7. copyList():  iterative version
    8. copyList():  recursive version
+   9. deAllocate():don't forget recursive version
+   10 insert():    in sorted order, iterative version, pending
+   11 insert():    in sorted order, recursive version, pending
+   12 deleteFromList():   pending
  */
 
+/* Nick's linked list problems
+   1. count(): return the number of times an int occurs in a list
+   2. getNth(): return the nth element of the list
+   3. deleteList(): deallocates all the memory and sets head to null
+   4. pop():    return head node data, delete head node, makes head node point to next
+ */
 
 
 #include <cctype>
@@ -36,18 +46,28 @@ void addToTail(node *head, int data);
 void printList(node *head);
 node *copyList(node *head);
 node *recCopyList(node *head);
+int count(node *head, int searchFor);
+int getNth(node *head, int index);
+void deleteList(node *head);
+void recDeleteList(node *head);
+int pop(node* & head);
+void insertNth(node* & head, int index, int data);
 
 int main()
 {
 
   node *testList = NULL;
   
-  testList = buildOneTwoThree();
-  addToTail(testList, 4);
+  //testList = buildOneTwoThree();
+  insertNth(testList, 0, 13);
+  insertNth(testList, 0, 100);
+
+  insertNth(testList, 1, 42);
+  
+  insertNth(testList, 1, 5);
+  insertNth(testList, 4, 27);
   printList(testList);
-  cout << endl;
-  node *copy = copyList(testList);
-  printList(copy);
+
 
 
   return 0;
@@ -173,6 +193,131 @@ node *copyList(node *head)
 
 node *recCopyList(node *head)
 {
-  
+  if (head == NULL) {
+    return NULL;
+  } else {
+    node *cur = new node;
+    cur -> data = head -> data;
+    cur -> next = recCopyList(head -> next);
+    return cur;
+  }
+}  
 
+int count(node *head, int searchFor)
+{
+  int count = 0;
+  for (node *cur = head; cur != NULL; cur = cur -> next) {
+    if ((cur -> data) == searchFor) {
+      count++;
+    }
+  }
+
+  return count;
+}  
+
+
+int getNth(node *head, int index)
+{
+
+  int count = 0;
+  for (node *cur = head; cur != NULL; cur = cur -> next) {
+    if (count == index) {
+      return cur -> data;
+    }
+    count++;
+  }
+
+  assert(0);  			// if we get here, then the client asked an index
+				// that was out of bounds
+}  
+
+
+void deleteList(node *head)
+{
+  while (head != NULL) {
+    node *next = head -> next;
+    delete head;
+    head = next;
+  }
+
+  head = NULL;
+}  
+
+
+// see how compact this is?
+void recDeleteList(node *head)
+{
+  if (head != NULL) {
+    recDeleteList(head -> next);
+    delete head;
+  }
+}  
+
+
+/* pop() returns the data head point to, delete that cell and move head
+ * to point to the next cell. Note head is passed by reference here so 
+ * you can modify it. 
+ */
+int pop(node* & head)
+{
+  assert (head != NULL);
+  int result = head -> data;
+  node* temp = head;
+  head = head -> next;
+  delete temp;
+  return result;
+}  
+
+
+/* insertNth() inserts a new cell with content of data at the Nth place of the list
+ * valid range of index is [0 .. length], pay attention to special cases such as
+ * empty list, head, and tail
+ * this was a bit tricky
+ * can I refactor this?
+ */
+void insertNth(node* & head, int index, int data)
+{
+  assert (index >=0 && index <= length(head));
+  node *cur = head;
+  int count = 0;
+
+  if (index == 0) {		// special case for adding the first cell
+    node *temp = head;
+    head = new node;
+    head -> data = data;
+    head -> next = temp;
+  } else {
+    while (count < index - 1) { // need to point at the position before the intend
+      cur = cur -> next;
+      count++;
+    }
+    node *temp = cur -> next;
+    cur -> next = new node;
+    (cur -> next) -> data = data;
+    (cur -> next) -> next = temp;
+  }
+
+  
+  // if (head == NULL) {		// special case for empty list
+  //   head = new node;
+  //   head -> data = data;
+  //   head -> next = NULL;
+  // } else {
+  //   if (index == 0) {		// special case for adding at first position
+  //     node *temp = head;
+  //     head = new node;
+  //     head -> data = data;
+  //     head -> next = temp;
+  //   } else {
+  //     while (count < index - 1) { // need to point at the position before the intend
+  // 	cur = cur -> next;
+  // 	count++;
+  //     }
+  //     node *temp = cur -> next;
+  //     cur -> next = new node;
+  //     (cur -> next) -> data = data;
+  //     (cur -> next) -> next = temp;
+  //   }
+  // }
+  
 }  
